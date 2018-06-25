a6fb3a9e32f1bef8ae051ad6587de6349347a68f
baozhourui <274024461@qq.com>
2018-06-25 21:34:50 +0800

    Add files via upload

---
+++ tiqu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from bs4 import BeautifulSoup
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+f1 = open("jinhua.html", "r")
+print "文件名: ", f1.name
+html=f1.read()
+print len(html)
+f1.close()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+
+answers=[]
+
+f2=open("answers.txt","a+")
+
+for i in (soup.find_all(attrs={'class':re.compile('topic-pp')})):
+    answers.append(i.get_text())
+
+answers.reverse()
+for j in answers:
+    f2.write(j + '\n')
+    f2.write('***********************************************************************************************' + '\n')
+f2.close()
